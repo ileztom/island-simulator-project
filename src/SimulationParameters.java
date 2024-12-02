2b7ecdd9be3ae2b6e4726208abe266fd2ac239dd
ileztom <91593668+ileztom@users.noreply.github.com>
2024-12-02 18:29:25 +0300

    Delete src directory

--- src/SimulationParameters.java
+++
@@ -1,5 +0,0 @@
-public class SimulationParameters {
-    public static final int ISLAND_WIDTH = 100;
-    public static final int ISLAND_HEIGHT = 20;
-    public static final int TICK_DURATION = 1;
-}
